35b6d5103f648940ff19d10af5d4810bb4550d5a
eyrum <eyrum@192.168.0.4>
2017-10-10 23:18:32 +0800

    ch8

--- ch8.scm
+++ ch8.scm
@@ -23,3 +23,45 @@
 ; function x (a) {return function(x) {return a === x}}
 ; var s = x(2)
 ; s(2)
+
+(define rember-f
+  (lambda (test?)
+    (lambda (a l)
+      (cond
+        ((null? l) (quote()))
+        ((test? (car l) a) (cdr l))
+        (else
+          (cons (car l) ((rember-f test?) a (cdr l)))
+        )
+      )
+    )
+  )
+)
+
+(define insertL-f
+  (lambda (test?)
+    (lambda (new old l)
+      (cond
+        ((null? l)(quote()))
+        ((test? (car l) old)(cons new (cons old (cdr l))))
+        (else
+          (cons (car l) ((insertL-f test?) new old (cdr l)))
+        )
+      )
+    )
+  )
+)
+
+(define insertR-f
+  (lambda (test?)
+    (lambda (new old l)
+      (cond
+        ((null? l)(quote()))
+        ((test? (car l) old)(cons old (cons new (cdr l))))
+        (else
+          (cons (car l) ((insertR-f test?) new old (cdr l)))
+        )
+      )
+    )
+  )
+)
